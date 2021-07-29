29355d01db1da43d16010b7619a3e1982e0f4296
waruru <c1005486@planet.kanazawa-it.ac.jp>
2021-07-29 13:47:24 +0900

    スペルミス修正

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
   echo "Hello World!";
   // ライブラリ読み込み
-  repuire_once __DIR__ . '/vendor/autoload.php';
+  require_once __DIR__ . '/vendor/autoload.php';
 
   // 値の取得
   $inputString = file_get_contents('php://input');
