c6a3223c7defaea6dab018098e1eeaed562ead3d
waruru <c1005486@planet.kanazawa-it.ac.jp>
2021-07-29 13:43:46 +0900

    チャット取得

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-  echo "Hello World!"
+  echo "Hello World!";
+  // ライブラリ読み込み
+  repuire_once __DIR__ . '/vendor/autoload.php';
+
+  // 値の取得
+  $inputString = file_get_contents('php://input');
+  echo $inputString;
+  error_log($inputString);
 ?>
