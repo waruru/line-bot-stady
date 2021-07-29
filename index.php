2d91580fced16de3bdf185d9b49b429df6006925
waruru <c1005486@planet.kanazawa-it.ac.jp>
2021-07-30 05:41:21 +0900

    POST内容をlogに表示

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
   // ライブラリ読み込み
   require_once __DIR__ . '/vendor/autoload.php';
 
+  // POST内容表示
+  $inputString = file_get_contents('php://input');
+  error_log($inputString);
+
   $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 
   $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
