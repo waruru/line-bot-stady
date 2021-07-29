32608e5b6746f95f6aeb6badf09a6f15daaddc97
waruru <c1005486@planet.kanazawa-it.ac.jp>
2021-07-30 04:24:38 +0900

    Message送信関数を作成

--- index.php
+++ index.php
@@ -12,6 +12,14 @@
 
   foreach($events as $event) {
     // テキスト返信
-    $bot->replyText($event->getReplyToken(), 'TextMessage');
+    $bot->replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
+  }
+
+  function replyTextMessage($bot, $replyToken, $text) {
+    $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
+
+    if(!$response->isSucceeded()) {
+      error_log('Failed! '. $response->getHTTPStatus. ' '. $response->getRawBody());
+    }
   }
 ?>
