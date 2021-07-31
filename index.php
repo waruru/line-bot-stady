f0b81ae282687cfc00b76328ade94d723b7579f9
waruru <c1005486@planet.kanazawa-it.ac.jp>
2021-07-31 09:34:45 +0900

    Eventによって返信内容を変更

--- index.php
+++ index.php
@@ -15,6 +15,10 @@
   $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
 
   foreach($events as $event) {
+    if ($event instanceof \LINE\LINEBot\Event\PostbackEvent) {
+      replyTextMessage($bot, $event->getReplyToken(), 'Postback受信「' . $event->getPostbackData() . '」');
+      continue;
+    }
     // テキストを返信
     // replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
 
